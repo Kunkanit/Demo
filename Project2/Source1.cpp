6c2d0895edc26451ac6a1c381c07746a4e8a58ed
USER <USER@LAPTOP-8UKKF9Q6>
2020-08-06 11:25:42 +0700

    Add project files.

---
+++ Project2/Source1.cpp
@@ -0,0 +1,16 @@
+#include"stdio.h"
+
+int main() {
+
+
+    float GBP, THB;
+
+
+
+    printf("Change Pound sterling to Thai Baht  \n");
+    printf("Input your GBP = ");
+    scanf("%f", &GBP);
+    printf("Your THB  = ", THB = GBP * 40.81);
+    printf("%.2f", THB);
+
+}
